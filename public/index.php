8faac2d418ff4821da305247beb43b2ac6e8f452
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-01-24 11:10:22 +0100

    init structure

--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
-<?php echo "Bienvenue sur leblog" ?>
+<?php
+
+session_start();
+date_default_timezone_set('Europe/Paris');
+if (!isset($_SESSION['count'])) {
+    $_SESSION['count'] = 0;
+} else {
+    $_SESSION['count']++;
+}
+
+if (!isset($_SESSION['dateFirstVisit'])) {
+    $_SESSION['dateFirstVisit'] = date("Y-m-d-H:i:s");
+}
+
+$id_session = session_id();
+
+
+$arg1 = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
+$file = $arg1 . ".php";
+$metaTitle = ucfirst($arg1);
+
+$routes = array(
+    "home" => $file,
+);
+
+
+ob_start();
+
+if(isset($routes[$arg1]))
+    require($routes[$arg1]);
+
+$render = ob_get_clean();
+echo $render;
