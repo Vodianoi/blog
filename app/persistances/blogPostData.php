e705ca4d8846a6a0e37ef810bb8855f4188675ff
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-01-24 14:29:16 +0100

    2.2.2 - 2.2.4

--- app/persistances/blogPostData.php
+++ app/persistances/blogPostData.php
@@ -3,5 +3,14 @@
 function lastBlogPosts($pdo) : array
 {
     $res = [];
+
+    $sql = file_get_contents('../database/lastBlogPosts.sql');
+    foreach ($pdo->query($sql) as $row) {
+        $res[] = array(
+            'title' => $row['title'],
+            'content' => $row['content'],
+            'author' => $row['nickname']
+        );
+    }
     return $res;
 }
