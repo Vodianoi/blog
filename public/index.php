aeef849d7e0d3d77ce8cf0858154554c06560f72
Axel Poty <axel.poty@le-campus-numerique.fr>
2024-01-24 13:21:13 +0100

    v2.1 Controller

--- public/index.php
+++ public/index.php
@@ -5,36 +5,16 @@ require __DIR__ . '/../autoload.php';
 
 (new App\Controller\DotEnvEnvironment)->load(__DIR__ . '/../');
 
-session_start();
-date_default_timezone_set('Europe/Paris');
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
-} else {
-    $_SESSION['count']++;
-}
 
-if (!isset($_SESSION['dateFirstVisit'])) {
-    $_SESSION['dateFirstVisit'] = date("Y-m-d-H:i:s");
-}
-
-$id_session = session_id();
-
-
-$arg1 = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
-$file = $arg1 . ".php";
-$metaTitle = ucfirst($arg1);
-
-include('../config/database.php');
-
-$routes = array(
-    "home" => __DIR__.$file,
-);
-
+//$arg1 = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_SPECIAL_CHARS);
+//$file = $arg1 . ".php";
+$controllerFolder = '/../app/controllers/';
+//$metaTitle = ucfirst($arg1);
 
 ob_start();
 
-if(isset($routes[$arg1]))
-    require($routes[$arg1]);
+if (empty($_GET))
+    require(__DIR__ . $controllerFolder . 'homeController.php');
 
 $render = ob_get_clean();
 echo $render;
